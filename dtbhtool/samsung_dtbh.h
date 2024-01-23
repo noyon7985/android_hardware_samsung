bcf4ee754780049d9e1ca2503384892016de5b4b
Soo-Hwan Na <whiteshell2544@naver.com>
2024-01-23 20:05:44 +0900

    samsung: dtbhtool: Just use working one

--- dtbhtool/samsung_dtbh.h
+++ dtbhtool/samsung_dtbh.h
@@ -1,6 +1,6 @@
 /* tools/mkbootimg/samsung_dtbh.h
 **
-** Copyright 2016, The LineageOS Project
+** Copyright 2017, The LineageOS Project
 **
 ** Licensed under the Apache License, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
@@ -18,19 +18,13 @@
 #ifndef _SAMSUNG_DTBH_H_
 #define _SAMSUNG_DTBH_H_
 
-/*
- * This is an example of how this header should look like.
- * You need to extract the values for your target manually.
- */
-
-#if 0 // DUMMY
 #define DTBH_MAGIC         "DTBH"
 #define DTBH_VERSION       2
-#define DTBH_PLATFORM      "k3g"
-#define DTBH_SUBTYPE       "k3g_eur_open"
-/* Hardcoded entry */
-#define DTBH_PLATFORM_CODE 0x1e92
-#define DTBH_SUBTYPE_CODE  0x7d64f612
+#define DTBH_PLATFORM      "android"
+#define DTBH_SUBTYPE       "samsung"
+ /* Hardcoded entry */
+#define DTBH_PLATFORM_CODE 0x000050a6
+#define DTBH_SUBTYPE_CODE  0x217584da
 
 /* DTBH_MAGIC + DTBH_VERSION + DTB counts */
 #define DT_HEADER_PHYS_SIZE 12
@@ -39,6 +33,5 @@
  * keep the eight uint32_t entries first in this struct so we can memcpy them to the file
  */
 #define DT_ENTRY_PHYS_SIZE (sizeof(uint32_t) * 8)
-#endif // DUMMY
 
 #endif // _SAMSUNG_DTBH_H_
